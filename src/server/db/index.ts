7f13e689f9278f5e84252baed90faf840adf53a8
moatazmohamedmm <moataz.mohamed@sylndr.com>
2024-05-22 11:16:26 +0300

    init

---
+++ src/server/db/index.ts
@@ -0,0 +1,8 @@
+import { drizzle } from "drizzle-orm/vercel-postgres";
+import { sql } from '@vercel/postgres';
+
+import { env } from "~/env";
+import * as schema from "./schema";
+
+
+export const db = drizzle(sql, { schema });
